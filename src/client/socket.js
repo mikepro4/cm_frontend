c699cc5f90d7903bc157d27fdbe1158e0b8d5111
Mikhail <pmp@nopreset.ru>
2021-03-03 16:02:18 -0500

    sockets working

--- src/client/socket.js
+++ src/client/socket.js
@@ -3,7 +3,7 @@ import socketIOClient from "socket.io-client";
 /////////////////////////////////////////////////
 
 export const io = () => {
-    return socketIOClient(window.BASE_API_URL  ?  "https://cashmachineapi.herokuapp.com" : "http://localhost:3100");
+    return socketIOClient(window.BASE_API_URL  ?  "https://cashmachineapi.herokuapp.com" : "http://localhost:5000");
 }
 
 /////////////////////////////////////////////////
